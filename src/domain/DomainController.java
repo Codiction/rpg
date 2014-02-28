bab153bf0d3dcb6f7cfde20b791cdcbdf47e5cac
Jenss21025 <jens_borgenon@hotmail.com>
2014-02-28 14:19:14 +0100

    class intializationV1

--- src/domain/DomainController.java
+++ src/domain/DomainController.java
@@ -1,10 +1,26 @@
 
 package domain;
 
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author A1
  */
 public class DomainController {
     
+    
+    public BufferedImage[] getMonsters(){
+        
+        throw new UnsupportedOperationException();
+    }
+    
+    public int [][] getChests(){
+        
+        throw new UnsupportedOperationException();
+    }
+    public int [][] getEffects(int id){
+        
+        throw new UnsupportedOperationException();
+    }
 }
---
+++ src/domain/MonsterRepository.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jens
+ */
+public class MonsterRepository {
+
+    public List<Monster> getMonster() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean linkMonster(Monster monster, Chest chest) {
+
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean saveMonster(Monster monster) {
+
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean deleteMonster(Monster monster){
+        
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean updateMonster(Monster monster){
+        
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean monsterExists(Monster moster){
+        
+        throw new UnsupportedOperationException();
+    }
+
+}
