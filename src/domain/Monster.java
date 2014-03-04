7680cb75b3eeaaa33b30933cc08b7cedf82ff344
A1 <>
2014-03-04 10:46:26 +0100

    update Monster & MonsterMapper

--- src/domain/Monster.java
+++ src/domain/Monster.java
@@ -1,4 +1,3 @@
-
 package domain;
 
 import java.util.ArrayList;
@@ -9,15 +8,89 @@ import java.util.List;
  * @author A1
  */
 public class Monster {
-    
+
     private ArrayList<Chest> guardedChests;
+    private String name;
+    private float power, defence, speed;
+    private int awareness;
+    private String avatarDir;
+    private int monsterId;
 
     public Monster() {
     }
-    
-    public List<Chest> getGuardedChests(){
+
+    public Monster(int monsterId, String name, float pow, float def, float sp, int awaren, String avDir) {
+        setName(name);
+        setPower(pow);
+        setDefence(def);
+        setSpeed(sp);
+        setAwareness(awaren);
+        setAvatarDir(avDir);
+        setMonsterId(monsterId);
+    }
+
+    public void setGuardedChests(ArrayList<Chest> chests) {
+        guardedChests = chests;
+    }
+
+    public List<Chest> getGuardedChests() {
         return guardedChests;
     }
-    
-    
+
+    public int getMonsterId() {
+        return monsterId;
+    }
+
+    public void setMonsterId(int monsterId) {
+        this.monsterId = monsterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPower() {
+        return power;
+    }
+
+    public void setPower(float power) {
+        this.power = power;
+    }
+
+    public float getDefence() {
+        return defence;
+    }
+
+    public void setDefence(float defence) {
+        this.defence = defence;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public int getAwareness() {
+        return awareness;
+    }
+
+    public void setAwareness(int awareness) {
+        this.awareness = awareness;
+    }
+
+    public String getAvatarDir() {
+        return avatarDir;
+    }
+
+    public void setAvatarDir(String avatarDir) {
+        this.avatarDir = avatarDir;
+    }
+
 }
--- src/persistence/mapper/MonsterMapper.java
+++ src/persistence/mapper/MonsterMapper.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,6 +19,11 @@ public class MonsterMapper extends Mapper {
 
     private final String qryGetMonsters = "SELECT * FROM monster";
     private final String qryGetGuardedChestByMonster = "SELECT treasureId FROM guardedchest WHERE monsterId = ?";
+    private final String qryGetMonster = "SELECT * FROM monster where monsterId = ?";
+
+    private final String qryDeleteMonster = "DELETE FROM monster WHERE monsterId = ?; DELETE FROM guardedchest WHERE monsterId = ?;";
+
+    private ArrayList<Monster> loadedMonsters = new ArrayList<>();
 
     public MonsterMapper(String dbLink) {
         super(dbLink);
@@ -24,26 +31,36 @@ public class MonsterMapper extends Mapper {
     }
 
     public ArrayList<Monster> loadMonsters() {
-        ArrayList<Monster> monsters = new ArrayList<>();
+        loadedMonsters.clear();
         openConnection();
         try {
             ResultSet rs = prepareStatement(qryGetMonsters).executeQuery();
             while (rs.next()) {
                 int monsterId = rs.getInt("monsterId");
-                Chest guardedChest = null;
+
                 PreparedStatement ps = prepareStatement(qryGetGuardedChestByMonster);
                 ps.setInt(1, monsterId);
                 ResultSet chestResults = ps.executeQuery();
 
-                //TODO: further handling (loop through all chest results, get the chest, link chest to newly created monster, add to monster list)
+                ArrayList<Chest> guardedChests = new ArrayList<>();
 
+                while (chestResults.next()) {
+                    Chest c = chestMapper.loadChest(chestResults.getInt("treasureId"));
+                    guardedChests.add(c);
+                }
+
+                Monster m = new Monster(rs.getInt("monsterId"), rs.getString("name"), rs.getFloat("power"),
+                        rs.getFloat("defence"), rs.getFloat("speed"), rs.getInt("awareness"), rs.getString("avatarDir"));
+                m.setGuardedChests(guardedChests);
+
+                loadedMonsters.add(m);
             }
         } catch (SQLException ex) {
             return null;
         } finally {
             closeConnection();
         }
-        return monsters;
+        return loadedMonsters;
     }
 
     public boolean saveMonster(Monster monster) {
@@ -55,14 +72,47 @@ public class MonsterMapper extends Mapper {
     }
 
     public boolean deleteMonster(Monster monster) {
-        throw new UnsupportedOperationException();
+        openConnection();
+        if (exists(monster)) {
+            PreparedStatement ps = prepareStatement(qryDeleteMonster);
+            try {
+                ps.setInt(1, monster.getMonsterId());
+                ps.setInt(2, monster.getMonsterId());
+                ps.executeUpdate();
+                return true;
+            } catch (SQLException ex) {
+                return false;
+            } finally {
+                closeConnection();
+            }
+        }
+        return false;
     }
 
     public boolean exists(Monster monster) {
-        throw new UnsupportedOperationException();
+        try {
+            PreparedStatement ps = prepareStatement(qryGetMonster);
+            ps.setInt(1, monster.getMonsterId());
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException se) {
+
+        }
+
+        return false;
     }
 
     public Monster loadMonster(int id) {
-        throw new UnsupportedOperationException();
+        //not sure if good like this or should directly load from database
+        loadMonsters();
+        for (Monster m : loadedMonsters) {
+            if (m.getMonsterId() == id) {
+                return m;
+            }
+        }
+        return null;
     }
 }
